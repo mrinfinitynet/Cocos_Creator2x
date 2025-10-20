ae0d21aad6ca967ee3c14d37a08350c33ccfc9ed
MR <masudranamd112@gmail.com>
2025-10-20 02:02:49 -0700

    ok

---
+++ src/settings.40c4d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/fruits_loop.fire",orientation:"",jsList:["assets/libs/socket.io.min.b601f.js"],bundleVers:{internal:"d17e2",resources:"d024d",main:"54b8f"}};
